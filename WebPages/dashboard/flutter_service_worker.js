9afdcf47e227fd9e6f42921306e9d15a0c9b8aac
Studer Alexander <alexander.studer@students.bfh.ch>
2026-05-16 09:58:06 +0200

    fix: implement CORS proxy routing for RAPT API

--- WebPages/dashboard/flutter_service_worker.js
+++ WebPages/dashboard/flutter_service_worker.js
@@ -3,12 +3,12 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "a8595b1d8b1b2005c1c60cc48342d781",
+const RESOURCES = {"flutter_bootstrap.js": "65da94e0352e80bb44f47bed1d4d4f7e",
 "isar.js": "d718516a62d0b962d0d9897c0a78cf32",
 "version.json": "4ea0ce02ad2eef6f9633ebaf0facd154",
 "index.html": "40ddf7fa4ba581a1a59c21c3d2123e1a",
 "/": "40ddf7fa4ba581a1a59c21c3d2123e1a",
-"main.dart.js": "eba1078199637c37c4227efad2c419cb",
+"main.dart.js": "e33a8e6ae65fde0d990bf762c6ae8400",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
